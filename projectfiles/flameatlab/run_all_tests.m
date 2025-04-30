e5406f496192549bd01ae18f06fb2820c1ea2fe6
Akshay Vemulapalli <akshayv@cs.utexas.edu>
2025-04-30 16:00:09 -0500

    cleaned up matlab driver

--- projectfiles/flameatlab/run_all_tests.m
+++ projectfiles/flameatlab/run_all_tests.m
@@ -4,61 +4,53 @@ pathstoadd = genpath('../../libflameatlab');
 addpath(pathstoadd);
 
 % Set matrix dimensions
-m = 6;
-n = 4;
+m = 36;
+n = 24;
 
 fprintf('\n==== Running FLAME Test Suite ====\n');
 
-% ----- Test 1: syrk_ln_blk_var1 -----
-A = randi([1,3], [m, n]);
-C_temp = randi([-3, 3], [m, m]);
-C1 = tril(C_temp) + tril(C_temp, -1)';
-C1 = tril(C1);
-expected1 = tril(A * (A.') + C1);
+% ----- Operation 1: syrk_ln -----
+A = randi([1,3], [m, n]); % random m x n matrix
+C = randi([-3, 3], [m, m]); % random m x m matrix
+C = tril(C); % C is a symmetric matrix, and we only store and update its lower triangle
 
-result1 = syrk_ln_blk_var1(A, C1);
-if isequal(result1, expected1)
+syrk_ln_expected_output = tril(A * (A.') + C); % we tril matlab's output to match our format of only the lower triangle being stored 
+
+% ----- Test 1: syrk_ln_blk_var1 -----
+test1_result = syrk_ln_blk_var1(A, C);\
+if isequal(test1_result, syrk_ln_expected_output)
     fprintf('All is well for : syrk_ln_blk_var1\n');
 else
     fprintf('Trouble in paradise : syrk_ln_blk_var1\n');
 end
 
 % ----- Test 2: syrk_ln_unb_var1 -----
-A = randi([1,3], [m, n]);
-C_temp = randi([-3, 3], [m, m]);
-C2 = tril(C_temp) + tril(C_temp, -1)';
-C2 = tril(C2);
-expected2 = tril(A * (A.') + C2);
-
-result2 = syrk_ln_unb_var1(A, C2);
-if isequal(result2, expected2)
+test2_result = syrk_ln_unb_var1(A, C);
+if isequal(test2_result, syrk_ln_expected_output)
     fprintf('All is well for : syrk_ln_unb_var1\n');
 else
     fprintf('Trouble in paradise : syrk_ln_unb_var1\n');
 end
 
-% ----- Test 3: trmm_lunn_blk_var1 -----
+% ----- Operation 2: trmm_lunn -----
 B = randi([1,3], [m, n]);
-U_temp = randi([-3, 3], [m, m]);
-U = triu(U_temp);
+U = randi([-3, 3], [m, m]);
+U = triu(U);
 
-expected3 = U * B;
-result3 = trmm_lunn_blk_var1(U, B);
-if isequal(result3, expected3)
+trmm_lunn_expected_output = U * B;
+
+% ----- Test 3: trmm_lunn_blk_var1 -----
+test3_result = trmm_lunn_blk_var1(U, B);
+if isequal(test3_result, trmm_lunn_expected_output)
     fprintf('All is well for : trmm_lunn_blk_var1\n');
 else
     fprintf('Trouble in paradise : trmm_lunn_blk_var1\n');
 end
 
 % ----- Test 4: trmm_lunn_unb_var1 -----
-B = randi([1,3], [m, n]);
-U_temp = randi([-3, 3], [m, m]);
-U = triu(U_temp);
-
-expected4 = (U * B);
-result4 = trmm_lunn_unb_var1(U, B);
-if isequal(result4, expected4)
-    fprintf('All is well for: trmm_lunn_unb_var1\n');
+test4_result = trmm_lunn_unb_var1(U, B);
+if isequal(test4_result, trmm_lunn_expected_output)
+    fprintf('All is well for : trmm_lunn_unb_var1\n');
 else
     fprintf('Trouble in paradise: trmm_lunn_unb_var1\n');
 end
